dda02f41114cbacbdfaa46fdb1ce3ec377840c5f
branches1 <105700994+branches1@users.noreply.github.com>
2022-08-10 08:21:59 +0100

    scratchfileadded

---
+++ scratch.py
@@ -0,0 +1,6 @@
+endings = {
+    'dealer_blackjack':
+    'The dealer got a blackjack. In this game that means a dealer win automatically, even if you also have blackjack. In other words: Suck it wagie, back in cagie.',
+    'player_blackjack':
+    "You got a blackjack and the dealer didn't. You win. Casino manager Hase will be round shortly to give you your winnings in a manner of his choosing."
+}
