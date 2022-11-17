318d144c85508fdf59328d373b5197ee6495cfe5
Acolls Badger <ancollis757@gmail.com>
2022-11-17 16:09:38 +0000

    change wording of bull.

--- grumpy_bull.py
+++ grumpy_bull.py
@@ -1,3 +1,3 @@
 def grumpy_bull():
 
-    print("You enter the Grumpy Bull's field to find him angrily trying to reach some lush grass through the fence. He looks at you and charges, driving you towards a pile of ripe manure.\nGAME OVER")
+    print("You enter the Grumpy Bull's field to find him angrily trying to reach some lush grass through the fence. He looks at you and charges, driving you towards a pile of the ripest manure.\nGAME OVER")
